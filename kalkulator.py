e23f2f02c97cf003133b5632c8ceb6ede5044e29
HekelPansos <69354104+HekelPansos@users.noreply.github.com>
2020-08-08 21:09:44 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,135 @@
+# coding: UTF-8
+# Author: ./Dark<Viper>
+# github: https://github.com/HekelPansos
+import random
+import sys
+import time
+import os
+def mengetik(s):
+      for c in s+'\n ' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random()*0.1)
+mengetik(" \033[1;36;5mSABAR INSTALL BAHAN DULU:v")
+time.sleep(1)
+mengetik("\033[1;36;5mLOADING...")
+time.sleep(2)
+os.system('clear')
+os.system('pkg install nano')
+os.system('pkg update && pkg upgrade')
+os.system('pip2 install requests')
+os.system('pkg install python2')
+os.system('pkg install python')
+os.system('clear')
+
+mengetik("\033[0;34;5m ASSALAMUALAIKUM.WR.WB")
+print
+time.sleep(1)
+mengetik("\033[1;33;5mJANGAN LUPA FOLLOW IG GW")
+print
+time.sleep(2)
+mengetik("\033[0;34;5m@ mr_viper22")
+print
+time.sleep(2)
+mengetik("\033[1;33;5mMAKASIH SAYANG")
+print
+time.sleep(2)
+mengetik("\033[0;34;5mBYE BYEE GUNAKAN DENGAN BAIK YAA")
+print
+time.sleep(3)
+print
+os.system('clear')
+logo = """\033[1;36;5m
+╦╔═╔═╗╦ ╦╔═╦ ╦╦ ╔═╗╔╦╗╔═╗╦═╗
+╠╩╗╠═╣║ ╠╩╗║ ║║ ╠═╣ ║ ║ ║╠╦╝
+╩ ╩╩ ╩╩═╝╩ ╩╚═╝╩═╝╩ ╩ ╩ ╚═╝╩╚═"""
+print (logo)
+print
+print
+print ('\033[0;34;5m====================================')
+print ('\033[0;35;5mAuthor: MR.VIPER || Risky.ID')
+print ('\033[0;35;5mIG.   : mr_viper22')
+print ('\033[0;35;5mWA.   : 083862851420')
+print ('\033[0;35;5mGC.   : Dark Cyber Team')
+print ('\033[0;34;5m====================================')
+print 
+print '\033[1;32;5mPilihan:'
+print
+print "\033[1;32;5m>>>>>>>>>>>>>>>>>>>"
+print '\033[0;34;5m[1].\033[1;33;5mPENJUMLAHAN'
+print "\033[1;32;5m<<<<<<<<<<<<<<<<<<<"
+print '\033[0;34;5m[2].\033[1;33;5mPENGURANGAN'
+print "\033[1;32;5m>>>>>>>>>>>>>>>>>>>"
+print '\033[0;34;5m[3].\033[1;33;5mPERKALIAN'
+print "\033[1;32;5m<<<<<<<<<<<<<<<<<<<"
+print '\033[0;34;5m[4].\033[1;33;5mPEMBAGIAN'
+print "\033[1;32;5m>>>>>>>>>>>>>>>>>>>"
+print '\033[0;34;5m[5].\033[1;33;5mSISABAGI'
+print "\033[1;32;5m<<<<<<<<<<<<<<<<<<<"
+print '\033[0;34;5m[6].\033[1;33;5mPEMANGKATAN'
+print "\033[1;32;5m>>>>>>>>>>>>>>>>>>>"
+print "\033[0;34;5m[0].\033[1;33;5mExit"
+print "\033[1;32;5m<<<<<<<<<<<<<<<<<<<"
+pilih = input('\033[0;34;5mPilih Menu: ')
+
+if pilih == 1:
+	print
+	angka_1 = input ('[Angka Pertama] => ')
+	angka_2 = input ('[Angka Kedua]   => ')
+	total = angka_1 + angka_2
+	print
+	print 'Totalnya :', total
+	
+if pilih == 2:
+	print
+	angka_1 = input ('[Angka Pertama] => ')
+	angka_2 = input ('[Angka Kedua]   => ')
+	total = angka_1 - angka_2
+	print
+	print 'Totalnya :', total
+
+if pilih == 3:
+	print
+	angka_1 = input ('[Angka Pertama] => ')
+	angka_2 = input ('[Angka Kedua]   => ')
+	total = angka_1 * angka_2
+	print
+	print 'Totalnya :', total
+	
+if  pilih == 4:
+	print
+	angka_1 = input ('[Angka Pertama] => ')
+	angka_2 = input ('[Angka Kedua]   => ')
+	total = angka_1 / angka_2
+	print
+	print 'Totalnya :', total
+	
+if pilih == 5:
+	print
+	angka_1 = input ('[Angka Pertama] => ')
+	angka_2 = input ('[Angka Kedua]   => ')
+	total = angka_1 % angka_2
+	print
+	print 'Totalnya :', total
+
+elif pilih ==6:
+	print
+	angka_1 = input ('[Angka Pertama] => ')
+	angka_2 = input ('[Angka Kedua]   => ')
+	total = angka_1 ** angka_2
+	print
+	print 'Totalnya :', total
+	
+if pilih ==00:
+	print
+	mengetik("\033[0;34;5mTERIMAKASIH UDAH MENGGUNAKAN SCRIPT GW")
+	exit
+	
+else:
+	print
+	print '__________________________'
+	print 'Menu Tidak Ada Goblok!!'
+	print '__________________________'
+	
+
+# Kalo Mau Di Salin Nama Gw Jangan Dihapus
